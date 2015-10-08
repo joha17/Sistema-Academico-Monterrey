bc3a0939818cf136d2e7e162ba9bca21105bceb1
joha17 <cervapz@gmail.com>
2015-10-08 12:21:26 -0600

    login modificado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :libros
-  resources :posts
 resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
